2f2f85554726955d5765a3e4a958b07fb939737b
James Gloudemans <james.gloudemans@gmail.com>
2020-12-11 16:41:46 -0500

    urdf from command line arg

--- ros_ws/src/jackal_gazebo/scripts/spawn_jackal.py
+++ ros_ws/src/jackal_gazebo/scripts/spawn_jackal.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
+import argparse
+import sys
+
 import rclpy
+
+
+def get_args():
+    """Get command line arguments."""
+    parser = argparse.ArgumentParser(description="Jackal spawner")
+    parser.add_argument(
+        'urdf', type=argparse.FileType(), help='The urdf of the robot to spawn.'
+    )
+    return parser.parse_args(sys.argv[1:])
 
 
 def main(args=None):
     rclpy.init(args=args)
+    urdf: str = get_args()
     node = rclpy.create_node('test_node')
-    node.get_logger().info('Ready!')
+    node.get_logger().info(f'{urdf}')
     rclpy.shutdown()
 
 
