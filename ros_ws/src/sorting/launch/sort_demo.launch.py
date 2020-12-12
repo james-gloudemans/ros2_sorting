93ba9df2fafa3a1182ae2ebdd129b169bf50024d
James Gloudemans <james.gloudemans@gmail.com>
2020-12-11 23:01:39 -0500

    implemented behavior tree param

--- ros_ws/src/sorting/launch/sort_demo.launch.py
+++ ros_ws/src/sorting/launch/sort_demo.launch.py
@@ -3,9 +3,16 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    bt_file = '/opt/ros/foxy/share/nav2_bt_navigator/behavior_trees/navigate_w_replanning_and_recovery.xml'
+    with open(bt_file, 'r') as f:
+        behavior_tree = f.read()
     return LaunchDescription(
         [
-            Node(package='sorting', executable='sorter'),
+            Node(
+                package='sorting',
+                executable='sorter',
+                parameters=[{'behavior_tree': behavior_tree}],
+            ),
             Node(package='sorting', executable='object_selector'),
             Node(package='sorting', executable='picker'),
             Node(package='sorting', executable='placer'),
--- ros_ws/src/sorting/sorting/sorter.py
+++ ros_ws/src/sorting/sorting/sorter.py
@@ -47,10 +47,13 @@ class Sorter(Node):
             "aborted",
         )  # There should be some way to translate goal status codes to english in rclpy
 
-        # TODO: make this a ROS parameter
-        self.bt: str = '/opt/ros/foxy/share/nav2_bt_navigator/behavior_trees/navigate_w_replanning_and_recovery.xml'
+        self.declare_parameter('behavior_tree')
+        self.bt: str = self.get_parameter(
+            'behavior_tree'
+        ).get_parameter_value().string_value
 
     async def sort(self) -> None:
+        self.get_logger().info(f'{self.bt}')
         await asyncio.sleep(10)
         self.get_logger().info('Sorting complete.')
         self._nav_client.destroy()
